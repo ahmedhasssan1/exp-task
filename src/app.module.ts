9a871f5c683d435e806c9ffd8228b091bb7d00ab
ahmedhasssan1 <ahmedkhel2003@gmail.com>
2025-09-10 18:35:00 +0300

    gor production

--- src/app.module.ts
+++ src/app.module.ts
@@ -43,13 +43,12 @@ dotenv.config();
     TypeOrmModule.forRoot({
       type: 'mysql',
       host: process.env.DATABASE_HOST,
-      port: 3306,
+      port: Number (process.env.DATABASE_PORT),
       username: process.env.DATABASE_USERNAME,
       password: process.env.DATABASE_PASSWORD,
-      database: process.env.DATABASE,
+      database: process.env.DATABASE_NAME,
       autoLoadEntities: true,
-      synchronize: true,
-
+      // synchronize: true,
     }),
 
     AdminModule,
